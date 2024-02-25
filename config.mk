4442db29ac9a70f7061b22bf5e9ca82d35822942
David Marble <drmarble1@gmail.com>
2024-02-25 16:54:26 -0600

    samsung_slsi-linaro/config: Guard with namespaces (1/6)
    Change-Id: I6fa48b293f876b64a1e2c2256399f08a6bb35090

---
+++ config.mk
@@ -0,0 +1,7 @@
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/samsung_slsi-linaro/exynos \
+    hardware/samsung_slsi-linaro/exynos5 \
+    hardware/samsung_slsi-linaro/graphics \
+    hardware/samsung_slsi-linaro/interfaces \
+    hardware/samsung_slsi-linaro/openmax
