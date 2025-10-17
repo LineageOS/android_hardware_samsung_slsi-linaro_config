133ef121407855b2d35d3226cd63fb264893e24a
ExtremeXT <extremextdev@gmail.com>
2025-10-17 16:08:02 +0300

    config: Include sgpu soong namespace
    Change-Id: I46390caa3677656f7ef0350b2a41ec4b597bdbd6
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- config.mk
+++ config.mk
@@ -20,4 +20,5 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/samsung_slsi-linaro/exynos5 \
     hardware/samsung_slsi-linaro/graphics \
     hardware/samsung_slsi-linaro/interfaces \
-    hardware/samsung_slsi-linaro/openmax
+    hardware/samsung_slsi-linaro/openmax \
+    hardware/samsung_slsi-linaro/sgpu
