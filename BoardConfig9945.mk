9828eb1ba8b2ee55f3c430618073fad00baf5557
Francescodario Cuzzocrea <bosconovic@gmail.com>
2025-10-26 23:56:32 +0100

    BoardConfig9945: rework 9945 configuration
    Change-Id: Ie36c03bf8ae35122ebf708e0e2670bdf70f28aa4
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- BoardConfig9945.mk
+++ BoardConfig9945.mk
@@ -14,24 +14,49 @@
 # limitations under the License.
 #
 
-TARGET_SOC_BASE := s5e9945
 TARGET_LINUX_KERNEL_VERSION := 6.1
 
-# Gralloc
-$(call soong_config_set,sgr,backend,sgpu)
-$(call soong_config_set,sgr,enable_sajc,true)
-$(call soong_config_set,arm_gralloc,gralloc_version,four_sgr)
+TARGET_SOC_BASE := s5e9945
 
-# HWC
-BOARD_USES_HDR_INTERFACE := true
-BOARD_USES_DISPLAY_COLOR_INTERFACE := true
-TARGET_USES_DISPLAY_RENDER_INTENTS := true
-HWC_SUPPORT_COLOR_TRANSFORM := true
-BOARD_USES_HWC_CPU_PERF_MODE := true
-$(call soong_config_set,exynos_hwc,uses_hwc_services,true)
-$(call soong_config_set,libacryl,default_scaler,mscl_sbwc_v2_7)
+# Surfaceflinger
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Gralloc
+BOARD_USES_EXYNOS_GRALLOC_VERSION := sgr
+BOARD_USES_4K_SWIZZLE_MODE := true
 
 # Codec2
-$(call soong_config_set,openmax,USE_GDC,true)
+BOARD_USE_CSC_FILTER := true
+BOARD_SUPPORT_MFC_ENC_RGB := true
+BOARD_USE_GDC := true
+BOARD_USE_HDR10PLUS_STAT_ENC := true
+BOARD_USE_FULL_ST2094_40 := true
+BOARD_HDR_DYNAMIC_META_LIB := librechdr10plus.plugin.so
+BOARD_HW_SUPPORT_FILMGRAIN := true
+BOARD_SUPPORT_MFC_ENC_BT2020 := true
+BOARD_GPU_TYPE := sgpu
+BOARD_USE_SUPPORT_GPU_SBWC := true
+BOARD_USE_FLEXIBLE_P010 := true
+
+# HWComposer
+BOARD_USES_DISPLAY_COLOR_INTERFACE := true
+BOARD_USES_EXYNOS_SAJC_FEATURE := true
+TARGET_USES_DISPLAY_RENDER_INTENTS := true
+HWC_SUPPORT_COLOR_TRANSFORM := true
+
+# HDR
+# This shall be enabled by device tree as it requires a wrapper
+# lib which is not part of BSP
+# BOARD_LIBHDR_PLUGIN := //device/samsung/s5e9945:libhdr_wrapper
+
+# WifiDisplay
+BOARD_USES_VIRTUAL_DISPLAY := true
+BOARD_USES_DISABLE_COMPOSITIONTYPE_GLES := true
+BOARD_USES_SECURE_ENCODER_ONLY := true
+
+# Acryl
+BOARD_USES_DEFAULT_LIBHDR_HEADER := true
+BOARD_LIBACRYL_DEFAULT_SCALER := mscl_sbwc_v2_8
+BOARD_SBWC_WRAPPER_PRIORITY := mscl
 
 include hardware/samsung_slsi-linaro/config/BoardConfigCommon.mk
