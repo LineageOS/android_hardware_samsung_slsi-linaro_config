a4aed1e9b8f4522c2f1e76abb68d0bcf332110c3
Atakan <ata.kaner03@gmail.com>
2026-01-23 23:51:40 +0300

    config: Add exynos2100 configuration
    Change-Id: I850d5aa42fe668b43bdeda78301be5bf821009d5

---
+++ BoardConfig2100.mk
@@ -0,0 +1,45 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_LINUX_KERNEL_VERSION := 5.4
+
+TARGET_SOC_BASE := exynos2100
+
+# Acryl
+BOARD_LIBACRYL_DEFAULT_SCALER := mscl_votf
+
+# Codec2
+BOARD_USE_CSC_FILTER := true
+BOARD_SUPPORT_MFC_ENC_RGB := true
+BOARD_USE_BLOB_ALLOCATOR := true
+BOARD_HW_SUPPORT_FILMGRAIN:= true
+
+# Gralloc
+BOARD_USES_EXYNOS_GRALLOC_VERSION := 4
+BOARD_USES_ALIGN_RESTRICTION := true
+BOARD_USES_GRALLOC_ION_SYNC := true
+BOARD_GPU_SUPPORT_AFBC_BASIC := true
+BOARD_GRALLOC_DPU_SUPPORT_1010102_AFBC := true
+BOARD_GRALLOC_INIT_AFBC := true
+BOARD_EXYNOS_S10B_FORMAT_ALIGN := 64
+
+# Graphics
+BOARD_USES_EXYNOS_DATASPACE_FEATURE := true
+
+# HWC
+BOARD_USES_EXYNOS_AFBC_FEATURE := true
+BOARD_LIBHDR_PLUGIN := //vendor/samsung/universal2100-common:vendor.samsung.libcolor.hardware
+BOARD_LIBHDR10P_META_PLUGIN := //vendor/samsung/universal2100-common:vendor.samsung.libcolor.hdr10plus
+
+# Libhwjpeg
+$(call soong_config_set,libhwjpeg,HWJPEG_ANDROID_VERSION,11)
+
+# WifiDisplay
+BOARD_USES_VIRTUAL_DISPLAY := true
+
+# Scaler
+BOARD_USES_SCALER_M2M1SHOT := true
+
+include hardware/samsung_slsi-linaro/config/BoardConfigCommon.mk
